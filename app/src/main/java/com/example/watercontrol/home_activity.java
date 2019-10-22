e84db8957112c7c1a8a6b75590299a0d13630a91
interceptar12 <jaimito1189@gmail.com>
2019-10-22 18:57:49 -0500

    Project with less Warnings

--- app/src/main/java/com/example/watercontrol/home_activity.java
+++ app/src/main/java/com/example/watercontrol/home_activity.java
@@ -28,7 +28,7 @@ public class home_activity extends AppCompatActivity {
         recordbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(home_activity.this, record_activity.class));
             }
         });
     }
