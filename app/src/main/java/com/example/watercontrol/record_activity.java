8fec509963cb57a15e1c6b3835cd5d14b40ab20c
interceptar12 <jaimito1189@gmail.com>
2019-10-22 19:36:20 -0500

    record_activity added (Incomplete)

--- app/src/main/java/com/example/watercontrol/record_activity.java
+++ app/src/main/java/com/example/watercontrol/record_activity.java
@@ -8,13 +8,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class record_activity extends AppCompatActivity {
+
+    Button gobackbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_activity);
+
+        //Declaración de variables
+        gobackbutton = findViewById(R.id.button_goback);
+
+        //Llamada de métodos
+        gobackbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(record_activity.this, home_activity.class));
+            }
+        });
     }
 
     @Override
