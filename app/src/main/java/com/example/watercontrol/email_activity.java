c1371d8ed815060a8181232fbfe7765576914926
interceptar12 <jaimito1189@gmail.com>
2019-11-07 14:38:00 -0600

    Recoverpassword Done

--- app/src/main/java/com/example/watercontrol/email_activity.java
+++ app/src/main/java/com/example/watercontrol/email_activity.java
@@ -1,20 +1,33 @@
 package com.example.watercontrol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class email_activity extends AppCompatActivity {
+    //Variable para poder ver el error en consola
+    private static final String TAG = "Error";
 
     Button passwordverify, gobackbutton;
+    ProgressDialog progressDialog;
     EditText edit_email;
     TextInputLayout input_email;
+
+    //Declaramos lo necesario para firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,9 @@ public class email_activity extends AppCompatActivity {
 
         setContentView(R.layout.activity_email_activity);
 
+        //Inicializamos el objeto Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         //Declaración de variables
         //Buttones
         passwordverify = findViewById(R.id.button_passwordverify);
@@ -44,15 +60,50 @@ public class email_activity extends AppCompatActivity {
         passwordverify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(email_activity.this, MainActivity.class));
+                recoverdPassword();
             }
         });
 
         gobackbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(email_activity.this, MainActivity.class));
+                finish();
             }
         });
+    }
+
+    private void recoverdPassword(){
+        startProgressdialog();
+        String emailtext = edit_email.getText().toString().trim();
+
+        mAuth.sendPasswordResetEmail(emailtext)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            endProgessdialog();
+                            Toast.makeText(email_activity.this, R.string.recoveremail_send,
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                        else{
+                            Log.w(TAG, "recoverPassword:failure", task.getException());;
+                            Toast.makeText(email_activity.this, R.string.recoveremail_failed,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    public void startProgressdialog(){
+        progressDialog = new ProgressDialog(email_activity.this);
+        progressDialog.setTitle("Recuperar Contraseña");
+        progressDialog.setMessage("...Enviando correo de Recuperación...");
+
+        progressDialog.show();
+    }
+
+    public void endProgessdialog(){
+        progressDialog.dismiss();
     }
 }
